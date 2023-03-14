e84a8065497239555ff3dced1edc9cc313bdb1e2
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-14 09:06:21 -0500

    New class to hide and show the turn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { Square } from "./components/Square";
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
+  X: 'X',
+  O: 'O'
 }
 
 function App() {
   const [ board, setBoard ] = useState(Array(9).fill(null))
-  console.log(board);
+  const [ turn, setTurn ] = useState(TURNS.X)
+  
+  const updateBoard = () => {
+    console.log('hello memo');
+  }
 
   return (
     <div className="App board">
@@ -15,11 +20,21 @@ function App() {
       <section className="game">
         {board.map((_, index) => {
           return (
-            <div className="square" key={index}>
+            <Square
+              className='square'
+              key={index}
+              updateBoard={updateBoard}
+              index={index}
+            >
               {_}
-            </div>
+            </Square>
           )
         })}
+      </section>
+
+      <section className="turn">
+        <Square className={turn === TURNS.X ? 'hide' : 'square block'}>{TURNS.X}</Square>
+        <Square className={turn === TURNS.O ? 'hide' : 'square block'}>{TURNS.O}</Square>
       </section>
     </div>
   );
