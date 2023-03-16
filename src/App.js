583c007ffc5584db1aa169bed15e834ee7bcc000
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-16 11:26:05 -0500

    Adding the text when one user wins

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import { useState } from "react";
 import { Square } from "./components/Square";
-
-const TURNS = {
-  X: 'X',
-  O: 'O'
-}
+import { TURNS, CHANCES_TO_WIN } from "./constants/constants"
 
 function App() {
   const [ board, setBoard ] = useState(Array(9).fill(null))
   const [ turn, setTurn ] = useState(TURNS.X)
   const [ winner, setWinner ] = useState(null)
+
+  const isThereWinner = (squareToCheck) => {
+    for (const tricky of CHANCES_TO_WIN) {
+      const [a, b, c] = tricky
+      if (
+        squareToCheck[a] &&
+        squareToCheck[a] === squareToCheck[b] &&
+        squareToCheck[b] === squareToCheck[c]
+      ) {
+        return squareToCheck[a]
+      }
+    }
+    return null
+  }
   
   const updateBoard = (index) => {
     if(board[index] || winner) return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
     const nextTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(nextTurn)
 
-    if (newBoard[0] === TURNS.X & newBoard[1] === TURNS.X & newBoard[2] === TURNS.X || newBoard[0] === TURNS.O & newBoard[1] === TURNS.O & newBoard[2] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[3] === TURNS.X & newBoard[4] === TURNS.X & newBoard[5] === TURNS.X || newBoard[3] === TURNS.O & newBoard[4] === TURNS.O & newBoard[5] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[6] === TURNS.X & newBoard[7] === TURNS.X & newBoard[8] === TURNS.X || newBoard[6] === TURNS.O & newBoard[7] === TURNS.O & newBoard[8] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[0] === TURNS.X & newBoard[3] === TURNS.X & newBoard[6] === TURNS.X || newBoard[0] === TURNS.O & newBoard[3] === TURNS.O & newBoard[6] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[1] === TURNS.X & newBoard[4] === TURNS.X & newBoard[7] === TURNS.X || newBoard[1] === TURNS.O & newBoard[4] === TURNS.O & newBoard[7] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[2] === TURNS.X & newBoard[5] === TURNS.X & newBoard[8] === TURNS.X || newBoard[2] === TURNS.O & newBoard[5] === TURNS.O & newBoard[8] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[0] === TURNS.X & newBoard[4] === TURNS.X & newBoard[8] === TURNS.X || newBoard[0] === TURNS.O & newBoard[4] === TURNS.O & newBoard[8] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
-    } else if (newBoard[2] === TURNS.X & newBoard[4] === TURNS.X & newBoard[6] === TURNS.X || newBoard[2] === TURNS.O & newBoard[4] === TURNS.O & newBoard[6] === TURNS.O) {
-      setWinner(turn)
-      console.log(`${turn} is the winner`)
+    const isWinner = isThereWinner(newBoard)
+
+    if (isWinner) {
+      setWinner(isWinner)
     }
   }
 
@@ -65,9 +56,14 @@ function App() {
       </section>
 
       <section className="turn">
-        <p>The next turn is for: </p>
-        <Square className={turn === TURNS.X ? 'square block' : 'hide'}>{TURNS.X}</Square>
-        <Square className={turn === TURNS.O ? 'square block' : 'hide'}>{TURNS.O}</Square>
+        {winner ? 
+          <p className="is-winner">The winner is: <strong> {winner} </strong></p> :
+          <>
+            <p className="is-winner">The next turn is for: </p>
+            <Square className={turn === TURNS.X ? 'square block' : 'hide'}>{TURNS.X}</Square>
+            <Square className={turn === TURNS.O ? 'square block' : 'hide'}>{TURNS.O}</Square>
+          </>
+        }
       </section>
     </div>
   );
