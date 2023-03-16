c564b5ab5881d52a2116cc9e38f47a95c1836187
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-03-15 19:51:53 -0500

    Adding the option to stop the game if one user wins

--- src/App.js
+++ src/App.js
@@ -9,14 +9,41 @@ const TURNS = {
 function App() {
   const [ board, setBoard ] = useState(Array(9).fill(null))
   const [ turn, setTurn ] = useState(TURNS.X)
+  const [ winner, setWinner ] = useState(null)
   
   const updateBoard = (index) => {
-    if(board[index]) return
+    if(board[index] || winner) return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
     const nextTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(nextTurn)
+
+    if (newBoard[0] === TURNS.X & newBoard[1] === TURNS.X & newBoard[2] === TURNS.X || newBoard[0] === TURNS.O & newBoard[1] === TURNS.O & newBoard[2] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[3] === TURNS.X & newBoard[4] === TURNS.X & newBoard[5] === TURNS.X || newBoard[3] === TURNS.O & newBoard[4] === TURNS.O & newBoard[5] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[6] === TURNS.X & newBoard[7] === TURNS.X & newBoard[8] === TURNS.X || newBoard[6] === TURNS.O & newBoard[7] === TURNS.O & newBoard[8] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[0] === TURNS.X & newBoard[3] === TURNS.X & newBoard[6] === TURNS.X || newBoard[0] === TURNS.O & newBoard[3] === TURNS.O & newBoard[6] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[1] === TURNS.X & newBoard[4] === TURNS.X & newBoard[7] === TURNS.X || newBoard[1] === TURNS.O & newBoard[4] === TURNS.O & newBoard[7] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[2] === TURNS.X & newBoard[5] === TURNS.X & newBoard[8] === TURNS.X || newBoard[2] === TURNS.O & newBoard[5] === TURNS.O & newBoard[8] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[0] === TURNS.X & newBoard[4] === TURNS.X & newBoard[8] === TURNS.X || newBoard[0] === TURNS.O & newBoard[4] === TURNS.O & newBoard[8] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    } else if (newBoard[2] === TURNS.X & newBoard[4] === TURNS.X & newBoard[6] === TURNS.X || newBoard[2] === TURNS.O & newBoard[4] === TURNS.O & newBoard[6] === TURNS.O) {
+      setWinner(turn)
+      console.log(`${turn} is the winner`)
+    }
   }
 
   return (
@@ -38,6 +65,7 @@ function App() {
       </section>
 
       <section className="turn">
+        <p>The next turn is for: </p>
         <Square className={turn === TURNS.X ? 'square block' : 'hide'}>{TURNS.X}</Square>
         <Square className={turn === TURNS.O ? 'square block' : 'hide'}>{TURNS.O}</Square>
       </section>
